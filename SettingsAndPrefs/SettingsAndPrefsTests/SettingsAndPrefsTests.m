51ce46a9fc0b038f377d258775bb3635d386c4f2
Robert DeNicola <thedizzle23@gmail.com>
2012-06-10 10:35:35 -0400

    here we are

---
+++ SettingsAndPrefs/SettingsAndPrefsTests/SettingsAndPrefsTests.m
@@ -0,0 +1,32 @@
+//
+//  SettingsAndPrefsTests.m
+//  SettingsAndPrefsTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "SettingsAndPrefsTests.h"
+
+@implementation SettingsAndPrefsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SettingsAndPrefsTests");
+}
+
+@end
