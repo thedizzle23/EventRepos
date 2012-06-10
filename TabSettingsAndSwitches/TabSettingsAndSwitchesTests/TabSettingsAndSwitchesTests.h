0bb5eac73be3847cc6f35a09c98a7b1e5656352f
Robert DeNicola <thedizzle23@gmail.com>
2012-06-10 09:26:10 -0400

    HERE IS COMMIT ONE

---
+++ TabSettingsAndSwitches/TabSettingsAndSwitchesTests/TabSettingsAndSwitchesTests.h
@@ -0,0 +1,13 @@
+//
+//  TabSettingsAndSwitchesTests.h
+//  TabSettingsAndSwitchesTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TabSettingsAndSwitchesTests : SenTestCase
+
+@end
---
+++ TabSettingsAndSwitches/TabSettingsAndSwitchesTests/TabSettingsAndSwitchesTests.m
@@ -0,0 +1,32 @@
+//
+//  TabSettingsAndSwitchesTests.m
+//  TabSettingsAndSwitchesTests
+//
+//  Created by Robert DeNicola on 6/10/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "TabSettingsAndSwitchesTests.h"
+
+@implementation TabSettingsAndSwitchesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TabSettingsAndSwitchesTests");
+}
+
+@end
